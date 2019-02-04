bb3a2129d0fb4109c04621cbce14bbdcc76f2098
Olaf Alders <olaf@wundersolutions.com>
2019-02-04 17:46:09 -0500

    Use custom type library

---
+++ lib/HTML/Restrict/Types.pm
@@ -0,0 +1,21 @@
+package HTML::Restrict::Types;
+
+use strict;
+use warnings;
+
+use Type::Library -base;
+use Type::Utils 1.000001 ();
+
+BEGIN {
+    Type::Utils::extends( 'Types::Common::Numeric', 'Types::Standard', );
+}
+
+__PACKAGE__->add_type(
+    {
+        name       => 'MaxParserLoops',
+        parent     => PositiveInt,
+        constraint => '$_ >= 2',
+    }
+);
+
+1;
